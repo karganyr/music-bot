57754d93526b4b431b2c59767ffd1bf7042f2794
Kshitij Upmanyu <karganyr@gmail.com>
2020-04-27 22:48:26 +0100

    Test

--- bot.js
+++ bot.js
@@ -316,8 +316,9 @@ function play(guild, song) {
       else if (serverQueue.loopall) {
         serverQueue.loopsongs.push(song);
         serverQueue.songs.shift();
-        message.channel.send(`Length: ${serverQueue.songs.length}`);
+        serverQueue.textChannel.send(`Length: ${serverQueue.songs.length}`);
         if (serverQueue.songs.length == 0) {
+          serverQueue.textChannel.send(`It's a me mario`);
           serverQueue.songs = serverQueue.loopsongs.splice();
           serverQueue.loopsongs = []
         }
