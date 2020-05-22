304eef6e5a03cd72808e379a6d750b0ca139ad46
Kshitij Upmanyu <karganyr@gmail.com>
2020-05-22 16:27:24 +0100

    Test

--- bot.js
+++ bot.js
@@ -362,12 +362,8 @@ function play(guild, song) {
     .play(ytdl(song.url))
     .on("finish", () => {
       if (serverQueue.loop) {
-        const song2 = {
-          title: song.title,
-          url: song.video_url
-        };
-        serverQueue.songs.push(song2);
-        serverQueue.songs.shift();
+        serverQueue.textChannel.send(`url = ${song.url}`);
+        serverQueue.textChannel.send(`songs url = ${serverQueue.songs[0].url}`);
         play(guild, serverQueue.songs[0]);
       }
       else if (serverQueue.loopall) {
