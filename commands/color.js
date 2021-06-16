f6d069281482c2ad9ee77ad826f58e98b590cb1b
Kshitij Upmanyu <karganyr@gmail.com>
2021-06-16 18:51:59 +0530

    Added handling for no color input for color.js

--- commands/color.js
+++ commands/color.js
@@ -5,8 +5,11 @@ module.exports = {
     args: true,
     execute(message, args) {
       if (!message.mentions.roles.size) {
-  			return message.reply('you need to mention a role to change color for!');
+  			return message.reply('You need to mention a role to change color for!');
   		}
+      if (args.length != 2) {
+        return message.reply('You need to mention a color to change to!');
+      }
   		message.mentions.roles.first().setColor(args[1])
     	.then(updated => console.log(`Set color of role to ${updated.color}`))
     	.catch(console.error);
